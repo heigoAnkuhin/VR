2125a3ec8d535557a6f7acdd133fbe32c07ff8ad
Heigo Ankuhin <heigoAnkuhin@users.noreply.github.com>
2021-03-05 15:48:17 +0200

    Esimese tunni esimene pool

---
+++ page.php
@@ -0,0 +1,30 @@
+<?php
+	$myname = "Heigo Ankuhin";
+	$currenttime = date("d.m.Y H:i:s");
+	$timehtml = "\n <p>Lehe avamise hetkel oli: " .$currenttime .".</p>">
+	$semesterbegin = new DateTime("2021-1-25");
+	$semesterend = new DateTime("2021-6-30");
+	$semesterduration = $semesterbegin->diff($semesterend);
+	$semesterdurationdays = $semesterduration->format("%r%a");
+	$semesterdurhtml = "\n <p> 2021 kevadsemestri kestus on " .$semesterdurationdays ." päeva.</p>";
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>Veebirakendused ja nende loomine 2021</title>
+</head>
+<body>
+	<h1>
+	<?php
+		echo $myname;
+	?>
+	</h1>
+	<p>See leht on valminud õppetöö raames!</p>
+	<?php
+		echo $timehtml;
+		echo $semesterdurhtml;
+	?>
+</body>
+</html>
