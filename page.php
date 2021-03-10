b26769c4216ae92a75b85d7a6bce0c35e4c51f54
Heigo Ankuhin <heigoAnkuhin@users.noreply.github.com>
2021-03-10 14:53:54 +0200

    Valmis fail

--- page.php
+++ page.php
@@ -1,18 +1,24 @@
 <?php
 	$myname = "Heigo Ankuhin";
 	$currenttime = date("d.m.Y H:i:s");
-	$timehtml = "\n <p>Lehe avamise hetkel oli: " .$currenttime .".</p>">
 	$semesterbegin = new DateTime("2021-1-25");
 	$semesterend = new DateTime("2021-6-30");
 	$semesterduration = $semesterbegin->diff($semesterend);
 	$semesterdurationdays = $semesterduration->format("%r%a");
 	$semesterdurhtml = "\n <p> 2021 kevadsemestri kestus on " .$semesterdurationdays ." päeva.</p>";
 	$today = new DateTime("now");
+	$currentday = $today->format("N"); /* Tänasest kuupäevast nopin välja nädalapäeva numbrina */
+	$days = ["Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev"]; /* Massiiv eestikeelseteks nädalanimedeks */
+	$dayofweek = $days[$currentday-1]; /* Õige nädalapäeva välja noppimine eestikeelsest massiivist */
+	$timehtml = "\n <p>Lehe avamise hetkel oli: " .$dayofweek .", " .$currenttime .".</p>";
 	$fromsemesterbegin = $semesterbegin->diff($today);
 	$fromsemesterbegindays = $fromsemesterbegin->format("%r%a");
 	$semesterprogress = "\n" .'<p>Semester edeneb: <meter min="0" max="' .$semesterdurationdays .'" value="' .$fromsemesterbegindays .'"></meter>.</p>' ."\n";
 
-	if($fromsemesterbegindays <= $semesterdurationdays) {
+	if ($fromsemesterbegindays < 0) { /* Kõigepealt kontrollime kas semester on üldse alanud - kui on, siis kukume otse elseif-i, kui ei, siis väljastame vastava teksti */
+		$semesterprogress = "\n <p>Semester pole veel alanud.</p> \n";
+	}
+	elseif ($fromsemesterbegindays <= $semesterdurationdays) {
 		$semesterprogress = "\n" .'<p>Semester edeneb: <meter min="0" max="' .$semesterdurationdays .'" value="' .$fromsemesterbegindays .'"></meter>.</p>' ."\n";	
 	}
 	else {
@@ -25,6 +31,7 @@
 	//echo $allfiles[5];
 	$allowedphototypes = ["image/jpeg", "image/png"];
 	$picfiles = [];
+	$photonums = [];
 	
 	foreach($allfiles as $file) {
 			$fileinfo = getimagesize($picsdir .$file);
@@ -37,9 +44,18 @@
 	}
 	
 	$photocount = count($picfiles);
-	$photonum = mt_rand(0, $photocount-1);
-	$randomphoto = $picfiles[$photonum];
 	
+	/* While-tsükkel kordumatute piltide väljanoppimiseks */
+	do {
+		$photonum = mt_rand(0, $photocount-1); /* Valime suvalise pildi numbri */
+		if(!(in_array($photonum, $photonums))) { /* Kui pole juba massiivis, siis sobib ja jätkame - kui aga juba on, siis hakkab tsükkel otsast peale ning valitakse uus pilt ja kontrollitakse uuesti */
+			array_push($photonums, $photonum); /* Lisame pildi numbri massiivi */
+		}
+	} while (count($photonums) < 3); /* Tsükkel käib niikaua kuni massiivi on lisatud 3 väärtust */
+	
+	$randomphoto = $picfiles[$photonums[0]];
+	$randomphoto2 = $picfiles[$photonums[1]];
+	$randomphoto3 = $picfiles[$photonums[2]];	
 ?>
 
 <!DOCTYPE html>
@@ -61,6 +77,9 @@
 		echo $semesterprogress;
 	?>
 	<img src="<?php echo $picsdir .$randomphoto; ?>" alt="vaade Haapsalus">
+	<img src="<?php echo $picsdir .$randomphoto2; ?>" alt="vaade Haapsalus 2">
+	<img src="<?php echo $picsdir .$randomphoto3; ?>" alt="vaade Haapsalus 3">
+
 	<!-- ../pics/IMG_0177.JPG" alt="vaade Haapsalus"> -->
 </body>
 </html>
